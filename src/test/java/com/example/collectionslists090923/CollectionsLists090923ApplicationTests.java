24b9f46851ac959d75a8c88665f08f6c3b299c06
air <kobychev00@bk.ru>
2023-09-25 19:41:49 +0300

    25.09.2023 19:41

---
+++ src/test/java/com/example/collectionslists090923/CollectionsLists090923ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collectionslists090923;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsLists090923ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
